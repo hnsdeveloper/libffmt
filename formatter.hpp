85f5a076d2187cc8e9faf0f9e8ccbcb47af8287a
Helio Nunes Santos <hnsantos@Helios-MacBook-Air.local>
2024-10-12 15:36:02 -0300

    Added printing oct and hex printing for negative numbers

--- formatter.hpp
+++ formatter.hpp
@@ -5,17 +5,20 @@ namespace hls
 {
 
     template <typename Integer, typename SinkImpl>
-    void unsigned_printer(Integer v, StreamSink<SinkImpl> &sink, const FormatSpecifier &fs, bool first)
+    void integer_printer(Integer v, StreamSink<SinkImpl> &sink, const FormatSpecifier &fs, bool first)
     {
         switch (fs.get_integer_display_type())
         {
             case OCT_FORMAT:
+            {
                 if (first)
                     sink.receive_data(char32_t('0'));
-                if (v / 8)
-                    unsigned_printer(v / 8, sink, fs, false);
-                sink.receive_data(char32_t('0') + (v % 8));
+                std::make_unsigned_t<Integer> p = v;
+                if (p / 8)
+                    integer_printer(p / 8, sink, fs, false);
+                sink.receive_data(char32_t('0') + (p % 8));
                 break;
+            }
             case DEC_FORMAT:
                 if (first)
                 {
@@ -43,23 +46,27 @@ namespace hls
                     }
                 }
                 if (v / 10)
-                    unsigned_printer(v / 10, sink, fs, false);
+                    integer_printer(v / 10, sink, fs, false);
                 v = v < 0 ? -(v % 10) : (v % 10);
                 sink.receive_data(char32_t('0') + v);
                 break;
             case HEX_FORMAT:
+            {
                 if (first)
                 {
                     sink.receive_data(char32_t('0'));
                     sink.receive_data(char32_t('x'));
                 }
-                if (v / 0x10)
-                    unsigned_printer(v / 0x10, sink, fs, false);
-                if (v % 0x10 < 0xA)
-                    sink.receive_data(char32_t('0') + v % 0x10);
+                // We don't care to the sign of the value, we just want the hex representation of it
+                std::make_unsigned_t<Integer> p = v;
+                if (p / 0x10)
+                    integer_printer(p / 0x10, sink, fs, false);
+                if (p % 0x10 < 0xA)
+                    sink.receive_data(char32_t('0') + p % 0x10);
                 else
-                    sink.receive_data(char32_t('A') + (v % 0x10) - 0xA);
+                    sink.receive_data(char32_t('A') + (p % 0x10) - 0xA);
                 break;
+            }
             default:
                 break;
         }
@@ -72,18 +79,12 @@ namespace hls
         template <typename SinkImpl>
         static void value_to_sink(const T &v, StreamSink<SinkImpl> &sink, const FormatSpecifier &fs)
         {
-
-            unsigned_printer(v, sink, fs, true);
-            /*
-            if constexpr (std::is_signed_v<T>)
-            {
-            }
-            else if constexpr (!std::is_signed_v<T>)
+            if constexpr (std::is_signed_v<T> || !std::is_signed_v<T>)
+                integer_printer(v, sink, fs, true);
             else
                 []<bool flag = false>() {
                     static_assert(flag, "Custom implementation needed for printing requested type.");
                 }();
-            */
         }
     };
 
